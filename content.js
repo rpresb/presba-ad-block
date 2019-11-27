f39af728f15a0093ae74d714c2b6a223b33133a6
Rodrigo De Presbiteris <rdepresbiteris@twilio.com>
2019-11-27 09:25:14 +0000

    first commit

---
+++ content.js
@@ -0,0 +1,20 @@
+const removeAd = () => {
+    const vlogad = document.querySelector('.vlog-ad');
+    if (vlogad) {
+        console.log('removed');
+        vlogad.remove();
+    } else {
+        setTimeout(removeAd, 1000);
+    }
+};
+
+document.querySelector('body').addEventListener('DOMNodeInserted', (e) => {
+    if (e.target.tagName === 'IFRAME') {
+        if (e.target.src.indexOf('https://multicanais.com/player.php') > -1) {
+            return;
+        }
+    }
+    e.target.remove();
+});
+
+removeAd();
